41dc9311435a2548bd06b36920eaf62d97b319ca
Ephraim Ben-Ishai <ephraim4321@gmail.com>
2024-07-14 00:46:47 +0300

    Addd uart-isr.c and uart-isr.h with rx isr functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# UART Driver with ISR
+## ISR for TX functionality only
+This driver uses an interrupt to transmit data
---
+++ uart_isr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "uart.h"
+
+#define GPIOAEN (1U << 0)
+#define UART2EN (1U << 17)
+
+#define DBG_UART_BAUDRATE   115200
+#define SYS_FREQ            16000000
+#define APB1_CLK            SYS_FREQ
+#define CR1_TEN             (1U << 3)
+#define CR1_UE              (1U << 13)
+#define SR_TXE              (1U << 7)
+
+
+void debug_uart_init(void){
+    //Enable clock accss to GPIOA
+    RCC->AHB1ENR |= GPIOAEN;
+}
